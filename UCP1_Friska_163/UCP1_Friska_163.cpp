b3b65bf7bd9fd03046c0a9f318bd8563e790dfc4
naxsyambis <naxsyambis@gmail.com>
2024-05-05 13:42:11 +0100

    Add project files.

---
+++ UCP1_Friska_163/UCP1_Friska_163.cpp
@@ -0,0 +1,46 @@
+// UCP1_Friska_163.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// 1. deklarasi variabel ada 3. Ada interger (Int) untuk bilangan bulat, float untuk bilangan berkoma, char variabel karakter
+// 2. implementasi prosedur dan fungsi ketika kita ingin membuat sistem untuk menghitung luas balok
+// 3. implementasi looping ketika kita ingin memunculkan angka rendom yang dibatasi oleh array
+// 4. implementasi dari conditional statement dimana kita ingin menghitung variabel dengan beberapa operasi hitungan contoh ada penjumlahan
+// perkalian, pembagian dalam satu waktu. contoh ketika ingin mencari rata-rata
+// 5. implementasi dari struck adalah untuk mengimput data mahasiswa umy 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int npolutan1, npolutan2, npolutan3;
+
+struct DetaiLokasi {
+    string Kota;
+};
+
+struct StatusKualitas {
+    string Status;
+};
+
+void dataA1() {
+    cout << "Masukkan nilai sensor alat 1 : ";
+    cin >> npolutan1;
+    
+}
+
+void dataA2() {
+    cout << "Masukkan nilai sensor alat 2 : ";
+    cin >> npolutan2;
+}
+
+void dataA3() {
+    cout << "Masukkan nilai sensor alat 3 : ";
+    cin >> npolutan3;
+}
+
+void Kudara() {
+    
+}
+
+int main() {
+
+}
+
